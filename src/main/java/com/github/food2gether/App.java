40bb74dd1dfe382b770e9ad96b0b7af2b413e51e
Marvin Haase <contact@marfien.dev>
2025-01-13 09:22:50 +0100

    Setup maven

---
+++ src/main/java/com/github/food2gether/App.java
@@ -0,0 +1,10 @@
+package src.main.java.com.github.food2gether;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/github/food2gether/AppTest.java
@@ -0,0 +1,19 @@
+package src.test.java.com.github.food2gether;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
