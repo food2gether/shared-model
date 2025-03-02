069f770fb7487a83aa9263e4e4b60582d7e0661f
Marfien <contact@marfien.dev>
2025-03-02 18:53:36 +0100

    feat: register for reflection constant

--- src/main/java/com/github/food2gether/shared/Constant.java
+++ src/main/java/com/github/food2gether/shared/Constant.java
@@ -1,7 +1,23 @@
 package com.github.food2gether.shared;
 
+import com.github.food2gether.shared.model.*;
+import com.github.food2gether.shared.response.DataAPIResponse;
+import com.github.food2gether.shared.response.ErrorAPIResponse;
+
 public class Constant {
 
     public static final String USER_MAIL_HEADER = "X-User-Mail";
+
+    public static final Class<?>[] REGISTER_FOR_REFLECTION = {
+            ContactInformation.DTO.class,
+            MenuItem.DTO.class,
+            Order.DTO.class,
+            OrderItem.DTO.class,
+            Profile.DTO.class,
+            Restaurant.DTO.class,
+            Session.DTO.class,
+            DataAPIResponse.class,
+            ErrorAPIResponse.class
+    };
 
 }
